7e665f8a243dede6f254826a933d58a213339e8b
zaighumhassan6 <zaighumhassan6@gmail.com>
2024-03-13 17:22:33 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
