5d0ef834788ec847a29bc4b3c160e98905dec015
zaighumhassan6 <zaighumhassan6@gmail.com>
2024-03-28 22:28:23 +0500

    Owner Added Complete

--- Controllers/OwnerController.cs
+++ Controllers/OwnerController.cs
@@ -1,154 +1,24 @@
-﻿using Asp.NetProject.Models;
+﻿using Asp.NetProject.Controllers;
+using Asp.NetProject.Models;
 using Microsoft.AspNetCore.Mvc;
-
 
 namespace Asp.NetProject.Controllers
 {
-    public class StoreController : Controller
+    public class OwnerController : Controller
     {
-
         private readonly IWebHostEnvironment webHostEnvironment;
         private readonly PosContext _dbcontext;
-        public StoreController(PosContext dbcontext, IWebHostEnvironment hostEnvironment)
+        public OwnerController(PosContext context, IWebHostEnvironment hostEnvironment)
         {
-
-            _dbcontext = dbcontext;
+            _dbcontext = context;
             webHostEnvironment = hostEnvironment;
         }
-        public IActionResult Index()
-        {
-            ViewBag.SMeesage = TempData["SMessage"];
-            ViewBag.EMessage = TempData["EMessage"];
-            return View(_dbcontext.Stores.ToList());
-        }
-
-        #region delete Store
-
 
         [HttpGet]
-        public IActionResult DeleteStore(int id)
-        {
-            try
-            {
-                Store store = _dbcontext.Stores.Find(id);
-                if(store!=null)
-                {
-                    _dbcontext.Stores.Remove(store);
-                    _dbcontext.SaveChanges();
-                    TempData["SMessage"] = "Record Deleted Successfully";
-                }
-                else
-                {
-                    TempData["EMessage"] = "Record Store  not found";
-                }
-            }
-            catch (Exception ex)
-            {
-                TempData["EMessage"] = "Some error occured";
-            }
-            return RedirectToAction(nameof(StoreController.Index));
-        
-            
-        }
-
-
-
-        #endregion delete Store 
-
-        #region store detail
-
-        [HttpGet]
-        public IActionResult StoreDetail(int id )
-        {
-            try
-            {
-                Store obj = _dbcontext.Stores.Find(id);
-                if(obj!=null)
-                {
-                    return View(obj);
-                }
-               
-            }
-            catch(Exception)
-            {
-
-            }
-
-
-            return View();
-        }
-
-
-
-        #endregion store detail
-
-
-        #region store update
-
-        [HttpGet]
-        public IActionResult UpdateStore(int id)
-        {
-            try
-            {
-
-                ViewBag.SMeesage = TempData["SMessage"];
-                ViewBag.EMessage = TempData["EMessage"];
-
-                Store obj = _dbcontext.Stores.Find(id);
-                if (obj == null)
-                {
-                    return View("No Record Found");
-                }
-                return View(obj);
-
-
-
-            }catch(Exception)
-            {
-                return View();
-            }
-
-
-
-        }
-
-        [HttpPost]
-        public IActionResult UpdateStore(Store obj)
-        {
-
-            try
-            {
-
-
-                if(obj!=null)
-                {
-
-                    obj.Logo = UploadedFile(obj);
-                    obj.UpdatedAt = DateTime.Now;
-                    _dbcontext.Stores.Update(obj);
-                    _dbcontext.SaveChanges();
-                    TempData["SMessage"] = "Data Updated Successfully";
-                }
-
-
-            }catch(Exception)
-            {
-
-                TempData["EMessage"] = "Some error occured. please try again!";
-
-            }
-
-
-            return View();
-        }
-        #endregion store update
-
-        #region store creation 
-        [HttpGet]
-        public IActionResult AddStore()
+        public IActionResult AddOwner()
         {
             var countries = new List<string>{
-               
+
         "Afghanistan",
         "Albania",
         "Algeria",
@@ -348,34 +218,59 @@ namespace Asp.NetProject.Controllers
 
             ViewBag.Countries = countries;
             return View();
-        }
 
+        }
         [HttpPost]
-        public IActionResult AddStore(Store store)
+        public IActionResult AddOwner(Owner owner)
         {
+
             try
             {
-                //var a = from stre in _dbcontext.Stores where stre.StoreName.Contains("A")
-                //        select stre;              
-                store.Logo = UploadedFile(store);
-                store.StoreName = store.StoreName.ToUpper();
-                store.Description = store.Description.ToUpper();
-                store.Location = store.Location.ToUpper();
-                store.City = store.City.ToUpper();
-                store.Email = store.Email.ToUpper();
-                store.Website = store.Website.ToUpper();
 
-                _dbcontext.Stores.Add(store);
-                _dbcontext.SaveChanges();
-                ViewBag.SMessage = "Data saved successfully";
-            }
-            catch (Exception)
+                        owner.Address = owner.Address.ToUpper();
+                        owner.FirstName = owner.FirstName.ToUpper();
+                        owner.LastName = owner.LastName.ToUpper();
+                        owner.Email = owner.Email.ToUpper();
+                        owner.City = owner.City.ToUpper();
+                        owner.Country = owner.Country.ToUpper();
+                        owner.Gender = owner.Gender.ToUpper();
+                        owner.CreatedAt = DateTime.Now;
+
+                        owner.Image = UploadedFile(owner);
+
+                        _dbcontext.Owners.Add(owner);
+                        _dbcontext.SaveChanges();
+                        ViewBag.SMessage = "Account created";
+                    
+
+                
+
+
+
+
+            }catch(Exception)
             {
-                ViewBag.EMessage = "Some Error Occurred";
+
+                ViewBag.EMessage = "Some error occured please try again lator !";
+
             }
             return View();
         }
-        private string UploadedFile(Store model)
+
+
+
+
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+
+
+        private string UploadedFile(Owner model)
         {
             if (model.LogoFile != null)
             {
@@ -396,7 +291,5 @@ namespace Asp.NetProject.Controllers
             // If no file was uploaded, return null or appropriate default value
             return null;
         }
-        #endregion store creation
-
     }
 }
--- Models/Owner.cs
+++ Models/Owner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Asp.NetProject.Models;
 
@@ -27,11 +28,18 @@ public partial class Owner
 
     public string? PostalCode { get; set; }
 
-    public byte[]? Image { get; set; }
+//    public byte[]? Image { get; set; }
 
     public DateTime? CreatedAt { get; set; }
 
     public DateTime? UpdatedAt { get; set; }
 
     public virtual ICollection<Store> Stores { get; set; } = new List<Store>();
+
+    [NotMapped]
+    public IFormFile? LogoFile { get; set; }//ImageFile to get image as an input from the user
+    public String? Image { get; set; }//Image
+
+
+
 }
