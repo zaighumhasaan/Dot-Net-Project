c8d07b2becb26ae7288c49cb90d6151e2e094c08
zaighumhassan6 <zaighumhassan6@gmail.com>
2024-04-03 09:15:59 +0500

    Department Module Completed

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -14,13 +14,193 @@ namespace Asp.NetProject.Controllers
         }
 
 
+        #region Create 
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.SMessage = TempData["SMessage"];
+            ViewBag.EMessage = TempData["EMessage"];
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department obj)
+        {
+            try
+            {
+                int? storeId;
+                storeId = HttpContext.Session.GetInt32("StoreId");
+                if(storeId == null)
+                {
+                    ViewBag.EMessage = "some error occured please try again lator !";
+                    return View();
+                }
+                else
+                {
+
+
+                    Department dep = _dbcontext.Departments.FirstOrDefault(d => d.DepartmentName == obj.DepartmentName);
+                    if (dep == null)
+                    {
+
+                        obj.StoreId = storeId;
+                        obj.DepartmentName = obj.DepartmentName.ToUpper();
+                        obj.CreatedAt = DateTime.Now;
+                        obj.UpdatedAt = DateTime.Now;
+                        obj.Description = obj.Description.ToUpper();
+                        _dbcontext.Departments.Add(obj);
+                        _dbcontext.SaveChanges();
+                        TempData["SMessage"] = "Success";
+
+                        return RedirectToAction("Create", "Department");
+                    }
+                    else
+                    {
+                        TempData["EMessage"] = "department with this name  already exists !";
+                        return RedirectToAction("Create", "Department");
+                    }
+
+
+                }
+ 
+
+
+            }
+            catch(Exception)
+            {
+                TempData["EMessage"] = "some error occured please try again lator !";
+            }
+
+
+
+
+
+            return View();
+        }
+
+
+        #endregion Create 
+
+        #region Detail
+
+        public IActionResult Detail(int id)
+        {
+
+            try
+            {
+                Department dep = _dbcontext.Departments.Find(id);
+                if (dep != null)
+                {
+                    return View(dep);
+                }
+
+
+
+            }
+            catch (Exception)
+            {
+                TempData["EMessage"] = "some error occured please try again lator !";
+            }
+            return View();
+        }
+
+
+
+        #endregion Detail
+
+        #region Update
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            try
+            {
+                Department dep = _dbcontext.Departments.Find(id);
+               
+                if (dep != null)
+                {
+
+                    ViewBag.SMessage = TempData["SMessage"];
+                    return View(dep);
+                }
+                else
+                {
+
+                    ViewBag.EMessage = TempData["EMessage"];
+                    return View();
+                }
+
+            }
+            catch(Exception)
+            {
+                ViewBag.EMessage = TempData["EMessage"];
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(Department dep)
+        {
+            try
+            {
+               
+
+                if (dep!=null)
+                {
+                    dep.DepartmentName = dep.DepartmentName.ToUpper();
+                    dep.Description = dep.Description.ToUpper();
+                    dep.UpdatedAt = DateTime.Now;
+              
+                    _dbcontext.Departments.Update(dep);
+                    _dbcontext.SaveChanges();
+                    TempData["SMessage"] = "Updated";
+                    return RedirectToAction("Create","Department");
+                }
+                TempData["EMessage"] = "some error occured please try again lator !";
+                return RedirectToAction("Create", "Department");
+            }
+            catch(Exception)
+            {
+                TempData["EMessage"] = "some error occured please try again lator !";
+            }
+            return View();
+        }
+
+
+        #endregion Update
+
+        #region Delete
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                Department dep = _dbcontext.Departments.Find(id);
+                if(dep!=null)
+                {
+                    
+                    _dbcontext.Departments.Remove(dep);
+                    _dbcontext.SaveChanges();
+                    TempData["SMessage"] = "Deleted";
+                    return RedirectToAction("Index", "Department");
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+
+            return View();
+        }
+        #endregion Delete
 
 
 
         public IActionResult Index()
         {
-            return View();
+            ViewBag.SMessage = TempData["SMessage"];
+            ViewBag.EMessage = TempData["EMessage"];
+            return View(_dbcontext.Departments.ToList());
         }
     }
 }
